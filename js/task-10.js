a38b82423cac455c5b53c19dc4aaec75d5430e89
ANastya28 <guselnikova2803@gmail.com>
2023-06-09 19:13:08 +0300

    updated

--- js/task-10.js
+++ js/task-10.js
@@ -2,24 +2,26 @@ const createBoxButton = document.querySelector(`[data-create]`);
 const destroyBoxButton = document.querySelector(`[data-destroy]`);
 const divBoxes = document.querySelector(`#boxes`);
 const counter = document.querySelector(`input`);
-
-
-  
+ 
 createBoxButton.addEventListener("click", createBoxes);
 destroyBoxButton.addEventListener("click", destroyBoxes);
-counter.addEventListener("click", amount);
+counter.addEventListener("click", getAmoutn);
+
+function getAmoutn() {
+  let amount = +counter.value;
+  createBoxes(amount);
+}
 
 let step = 30;
 
 function createBoxes(amount) {
-  let markupEl = `<div</div>`;
+  let markupEl = document.createElement("div");
   step += 10;
   markupEl.style.width = `${step}px`;
   markupEl.style.heigh = `${step}px`;
   markupEl.style.backgroundColor = getRandomHexColor();
 
   divBoxes.insertAdjacentHTML("afterbegin", markupEl);
-
 }
 
 function destroyBoxes() {
@@ -32,4 +34,5 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-let amout = 
+
+
